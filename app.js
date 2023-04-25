f804a73209b4fa02c10bc17474ad970cd22b76c2
Endres2 <andresdhr99@gmail.com>
2023-04-24 20:41:32 -0400

    Add views folder with ejs, add logic to post metho that will increase to do list elemnts by the input of the user when submitting

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 const express = require("express")
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const e = require("express");
 
 const app = express()
-app.use(bodyParser.urlencoded({extended:true}));
 
+var items = ["Buy Food","Go Workout"];
 
-app.get("/",(req,res)=>{
-    res.send("hello")
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", 'ejs');
+
+app.get("/", (req, res) => {
+    var today = new Date();
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var day = weekday[today.getDay()];
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+    var day = today.toLocaleDateString("en-US",options)
+
+    // if(currentDay === 6 || currentDay === 0){
+    //     day = "Weekend";
+    // }else{
+    //     day = "Weekday";
+    // }
+
+    res.render("list", { kindOfDay: day, newListItems: items });
+
+})
+app.post("/", (req,res)=>{
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("Server running in port 3000")
 })
+
