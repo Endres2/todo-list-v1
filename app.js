63e788416e87e720ccd8613b9d88a99ee5093477
Endres2 <andresdhr99@gmail.com>
2023-04-24 21:52:01 -0400

    Add header and footer templates, add about page for example, add route to /work to have a different to do list for work items

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const bodyParser = require("body-parser");
-const e = require("express");
+const { render } = require("ejs");
+
 
 const app = express()
 
 var items = ["Buy Food","Go Workout"];
+var workItems = [];
 
 app.use(express.static("public"))
 
@@ -30,14 +32,28 @@ app.get("/", (req, res) => {
     //     day = "Weekday";
     // }
 
-    res.render("list", { kindOfDay: day, newListItems: items });
+    res.render("list", { listTitle: day, newListItems: items, route: "/"});
 
 })
+
+app.get("/work", (req,res)=>{
+    res.render("list", {listTitle: "Work List", newListItems:workItems, route: "/work"})
+})
+app.get("/about", (req,res)=>{
+    res.render("about")
+})
+
 app.post("/", (req,res)=>{
     var item = req.body.newItem;
     items.push(item);
     res.redirect("/");
 })
+app.post("/work", (req,res)=>{
+    var item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
+
 
 app.listen(3000, () => {
     console.log("Server running in port 3000")
